172dd3d21573b6484429a3518b098a907b0cf9e5
quackquavk <138971300+quackquavk@users.noreply.github.com>
2023-08-09 05:31:11 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [{
+    label: 'Sample Data',
+    data: [10, 25, 12, 35, 24, 30],
+    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+    borderColor: 'rgba(75, 192, 192, 1)',
+    borderWidth: 1
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+};
+
+const myChart = new Chart(ctx, config);
 
